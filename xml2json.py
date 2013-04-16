3e5ab2779579f2a380d6222b1b22233e39298dc0
Anuj Gupta <anuj@codesyrup.com>
2013-04-17 02:24:30 +0530

    Fixed dumb mistake + PEP-8 overhaul + some refactoring

--- xml2json.py
+++ xml2json.py
@@ -1,62 +1,69 @@
-﻿'''
-	xml2json.py
-	Kailash Nadh, http://nadh.in
-	December 2012
-	
-	License:		MIT License
-	Documentation:	http://nadh.in/code/xmlutils.py
-'''
+﻿"""
+    xml2json.py
+    Kailash Nadh, http://nadh.in
+    December 2012
+    
+    License:        MIT License
+    Documentation:    http://nadh.in/code/xmlutils.py
+"""
 
-import argparse, codecs, elementtree.ElementTree as et
+import codecs
+import lxml.etree as et
 import json
 
 
-# turn an ElementTree Element to a list
 def elem2list(elem):
-	block = {}
+    """
+    Convert an ElementTree element to a list
+    """
+    block = {}
 
-	# get the element's children
-	children=elem.getchildren()
+    # get the element's children
+    children = elem.getchildren()
 
-	if children:
-		cur = map(elem2list, children)
+    if children:
+        cur = map(elem2list, children)
 
-		# create meaningful lists
-		if elem[0].tag != elem[1].tag:		# [{a: 1}, {b: 2}, {c: 3}] => {a: 1, b: 2, c: 3}
-			cur =	dict(zip(
-								map(lambda e: e.keys()[0], cur),
-								map(lambda e: e.values()[0], cur)
-							))
-		else:								# [{a: 1}, {a: 2}, {a: 3}] => {a: [1, 2, 3]} 
-			cur = { elem[0].tag: map(lambda e: e.values()[0], cur) }
+        # create meaningful lists
+        if elem[0].tag != elem[1].tag:  # [{a: 1}, {b: 2}, {c: 3}] => {a: 1, b: 2, c: 3}
+            cur = dict(zip(
+                map(lambda e: e.keys()[0], cur),
+                map(lambda e: e.values()[0], cur)
+            ))
+        else:  # [{a: 1}, {a: 2}, {a: 3}] => {a: [1, 2, 3]}
+            cur = {elem[0].tag: [e.values()[0] for e in cur]}
 
-		block[elem.tag] = cur
-	else:
-		block[elem.tag] = elem.text.strip()
-	
-	return block
-
-
-# convert ElementTree Element (root) to json
-def xml2json(elem, pretty = True):
-	# if the given Element is not the root element, find it
-	if hasattr(elem, 'getroot'):
-		elem=elem.getroot()
-
-	return json.dumps(elem2list(elem), indent = (4 if pretty else None) )
+        block[elem.tag] = cur
+    else:
+        block[elem.tag] = elem.text.strip()
+    
+    return block
 
 
-# convert an xml file to a json file
-def xml2json_file(input, output = None, pretty = True, encoding='utf-8'):
-	context = et.iterparse(input, events=("start", "end"))
-	context = iter(context)
-	event, root = context.next()
+def xml2json(elem, pretty=True):
+    """
+    Convert an ElementTree Element (root) to json
+    """
+    # if the given Element is not the root element, find it
+    if hasattr(elem, 'getroot'):
+        elem = elem.getroot()
 
-	json = xml2json(root, pretty)
+    return json.dumps(elem2list(elem), indent=(4 if pretty else None))
 
-	# if an output filename is given, write to it, otherwise, return json
-	if output != None:
-		output = codecs.open(output, "w", encoding)
-		output.write(json)
-	else:
-		return json
+
+def xml2json_file(input_file, output=None, pretty=True, encoding='utf-8'):
+    """
+    Convert xml file to json
+    """
+    context = et.iterparse(input_file, events=("start", "end"))
+    context = iter(context)
+    event, root = context.next()
+
+    json = xml2json(root, pretty)
+
+    # if an output filename is given, write to it, otherwise, return json
+    if output is not None:
+        output = codecs.open(output, "w", encoding)
+        output.write(json)
+    else:
+        return json
--- xml2sql.py
+++ xml2sql.py
@@ -1,18 +1,23 @@
-﻿'''
-	xml2sql.py
-	Kailash Nadh, http://kailashnadh.name
-	October 2011
-	
-	License:		MIT License
-	Documentation:	http://kailashnadh.name/code/xmlutils.py
-'''
+﻿"""
+    xml2sql.py
+    Kailash Nadh, http://nadh.in
+    October 2011
+    
+    License:        MIT License
+    Documentation:    http://nadh.in/code/xmlutils.py
+"""
 
-import argparse, codecs, elementtree.ElementTree as et
+import argparse
+import codecs
+import xml.etree.ElementTree as et
 
-print "xml2sql.py by Kailash Nadh (http://kailashnadh.name)\n--help for help\n\n"
+print """xml2sql.py by Kailash Nadh (http://nadh.in)
+--help for help
+
+"""
 
 # parse arguments
-parser = argparse.ArgumentParser(description='Process some integers.')
+parser = argparse.ArgumentParser(description='Convert an xml file to sql.')
 parser.add_argument('--input', type=file, dest='input_file', required=True, help='input xml filename')
 parser.add_argument('--output', dest='output_file', required=True, help='output sql filename')
 parser.add_argument('--tag', dest='tag', required=True, help='the record tag. eg: item')
@@ -20,9 +25,8 @@ parser.add_argument('--table', dest='table', required=True, help='table name')
 parser.add_argument('--ignore', dest='ignore', default='', nargs='+', help='list of tags to ignore')
 parser.add_argument('--encoding', dest='encoding', default='utf-8', help='character encoding (default=utf-8)')
 parser.add_argument('--limit', type=int, dest='limit', default=-1, help='maximum number of records to process')
-parser.add_argument('--packet', type=float, dest='packet', default='8', help='maximum size of an insert query in MB. see MySQL\'s max_allowed_packet (default=8)')
+parser.add_argument('--packet', type=float, dest='packet', default='8', help=r'maximum size of an insert query in MB. see MySQL\'s max_allowed_packet (default=8)')
 args = parser.parse_args()
-
 
 # output file handle
 output = codecs.open(args.output_file, "w", encoding=args.encoding)
@@ -30,76 +34,85 @@ output = codecs.open(args.output_file, "w", encoding=args.encoding)
 # open the xml file for iteration
 context = et.iterparse(args.input_file, events=("start", "end"))
 context = iter(context)
+
 # get to the root
 event, root = context.next()
 
+items = []
+tags = []
+output_buffer = []
 
-max_packet = 1048576 * args.packet
-
-items = []; tags = []; output_buffer = []
 tagged = False
 started = False
+
 sql_len = 0
 sql_insert = None
 num_insert = 0
 n = 0
+
 packet_size = 0
+max_packet = 1048576 * args.packet
 
-# write records to the output file
+
 def write_buffer():
-	global output_buffer, sql_insert, output, num_insert
-	
-	output.write(  sql_insert + 'VALUES\n' + ', \n'.join(output_buffer) + ';\n\n' )
-	output_buffer = []
-	num_insert += 1
+    """
+    Write records from buffer to the output file
+    """
+    global output_buffer, sql_insert, output, num_insert
 
-	print ".",
+    output.write(sql_insert + 'VALUES\n' + ', \n'.join(output_buffer) + ';\n\n')
+    output_buffer = []
+    num_insert += 1
+
+    print ".",
+
 
 def show_stats():
-	print "\n\nWrote", n, "records to", args.output_file, " (INSERT queries =", num_insert, ")"
+    print "\n\nWrote", n, "records to", args.output_file, " (INSERT queries =", num_insert, ")"
+
 
 # iterate through the xml
 for event, elem in context:
-	if event == 'start' and elem.tag == args.tag and not started:
-		started = True
+    if event == 'start' and elem.tag == args.tag and not started:
+        started = True
 
-	#child nodes of the specified record tag
-	if started and event == 'end' and elem.tag != args.tag and elem.tag not in args.ignore:
-		tags.append(elem.tag) if tagged == False else True	# field names
-		items.append( '-' if elem.text == None or elem.text.strip() == ''
-						 else elem.text.replace('"', '\\\"')
-									   .replace('\n', '\\n')
-									   .replace('\'', '\\\'')
-					)
-					
-	# end of traversing the record tag
-	if event == 'end' and elem.tag == args.tag and len(items) > 0:
-		tagged = True
+    elif event == 'end':
+        if started and elem.tag != args.tag and elem.tag not in args.ignore:
+        # child nodes of the specified record tag
+            if not tagged:
+                tags.append(elem.tag)  # add field names
+            if elem.text is None or elem.text.strip() == '':
+                items.append('-')
+            else:
+                items.append(elem.text.replace('"', r'\"').replace('\n', r'\n').replace('\'', r"\'"))
 
-		if sql_insert == None:
-			sql_insert = 'INSERT INTO ' + args.table + ' (' + ','.join(tags) + ')\n'
+        elif elem.tag == args.tag and len(items) > 0:
+        # end of traversing the record tag
+            tagged = True
 
-		sql = '(\"' + ('\", \"').join(items) + '\")'
-		sql_len += len(sql)
-		
-		# store the sql statement in the buffer
-		if sql_len+len(sql_insert)+100 < max_packet:
-			output_buffer.append(sql)
-		else:
-		# packet size exceeded. flush the sql and start a new insert query
-			write_buffer()
-			output_buffer.append(sql)
-			sql_len = 0
+            if sql_insert is None:
+                sql_insert = 'INSERT INTO ' + args.table + ' (' + ','.join(tags) + ')\n'
 
-		items = []
-		n+=1
-	
-	# halt if the specified limit has been hit
-	if n == args.limit:
-		break
+            sql = r'("' + r'", "'.join(items) + r'")'
+            sql_len += len(sql)
 
-	elem.clear() # discard element and recover memory
+            # store the sql statement in the buffer
+            if sql_len + len(sql_insert) + 100 < max_packet:
+                output_buffer.append(sql)
+            else:
+            # packet size exceeded. flush the sql and start a new insert query
+                write_buffer()
+                output_buffer.append(sql)
+                sql_len = 0
 
+            items = []
+            n += 1
+
+            # halt if the specified limit has been hit
+            if n == args.limit:
+                break
+
+        elem.clear()  # discard element and recover memory
 
 write_buffer()
 show_stats()
