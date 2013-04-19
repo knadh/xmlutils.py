958a75aa7d85a6625af57fcc25e15413d3630c31
Kailash Nadh <kailash.nadh@gmail.com>
2013-04-19 14:02:26 +0530

    Fixed a major bug in list comprehension

--- xml2json.py
+++ xml2json.py
@@ -1,10 +1,10 @@
 ﻿"""
-    xml2json.py
-    Kailash Nadh, http://nadh.in
-    December 2012
-    
-    License:        MIT License
-    Documentation:    http://nadh.in/code/xmlutils.py
+	xml2json.py
+	Kailash Nadh, http://nadh.in
+	December 2012
+	
+	License:        MIT License
+	Documentation:    http://nadh.in/code/xmlutils.py
 """
 
 import codecs
@@ -13,57 +13,58 @@ import json
 
 
 def elem2list(elem):
-    """
-    Convert an ElementTree element to a list
-    """
-    block = {}
+	"""
+	Convert an ElementTree element to a list
+	"""
+	block = {}
 
-    # get the element's children
-    children = elem.getchildren()
+	# get the element's children
+	children = elem.getchildren()
 
-    if children:
-        cur = map(elem2list, children)
+	if children:
+		cur = map(elem2list, children)
 
-        # create meaningful lists
-        if 1 not in elem or elem[0].tag != elem[1].tag:  # [{a: 1}, {b: 2}, {c: 3}] => {a: 1, b: 2, c: 3}
-            cur = dict(zip(
-                map(lambda e: e.keys()[0], cur),
-                map(lambda e: e.values()[0], cur)
-            ))
-        else:  # [{a: 1}, {a: 2}, {a: 3}] => {a: [1, 2, 3]}
-            cur = {elem[0].tag: [e.values()[0] for e in cur]}
+		# create meaningful lists
+		try:
+			if elem[0].tag != elem[1].tag:  # [{a: 1}, {b: 2}, {c: 3}] => {a: 1, b: 2, c: 3}
+				cur = dict(zip(
+					map(lambda e: e.keys()[0], cur),
+					map(lambda e: e.values()[0], cur)
+				))
+		except Exception as e:  # [{a: 1}, {a: 2}, {a: 3}] => {a: [1, 2, 3]}
+			cur = {elem[0].tag: [e.values()[0] for e in cur]}
 
-        block[elem.tag] = cur
-    else:
-        block[elem.tag] = elem.text.strip()
-    
-    return block
+		block[elem.tag] = cur
+	else:
+		block[elem.tag] = elem.text.strip() if elem.text else ""
+	
+	return block
 
 
 def xml2json(elem, pretty=True):
-    """
-    Convert an ElementTree Element (root) to json
-    """
-    # if the given Element is not the root element, find it
-    if hasattr(elem, 'getroot'):
-        elem = elem.getroot()
+	"""
+	Convert an ElementTree Element (root) to json
+	"""
+	# if the given Element is not the root element, find it
+	if hasattr(elem, 'getroot'):
+		elem = elem.getroot()
 
-    return json.dumps(elem2list(elem), indent=(4 if pretty else None))
+	return json.dumps(elem2list(elem), indent=(4 if pretty else None))
 
 
 def xml2json_file(input_file, output=None, pretty=True, encoding='utf-8'):
-    """
-    Convert xml file to json
-    """
-    context = et.iterparse(input_file, events=("start", "end"))
-    context = iter(context)
-    event, root = context.next()
+	"""
+	Convert xml file to json
+	"""
+	context = et.iterparse(input_file, events=("start", "end"))
+	context = iter(context)
+	event, root = context.next()
 
-    json = xml2json(root, pretty)
+	json = xml2json(root, pretty)
 
-    # if an output filename is given, write to it, otherwise, return json
-    if output is not None:
-        output = codecs.open(output, "w", encoding)
-        output.write(json)
-    else:
-        return json
+	# if an output filename is given, write to it, otherwise, return json
+	if output is not None:
+		output = codecs.open(output, "w", encoding)
+		output.write(json)
+	else:
+		return json
