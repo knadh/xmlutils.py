be03e327ebab0af1c9c66031843260d570811dc3
Kailash Nadh <kailash.nadh@gmail.com>
2013-06-03 23:56:36 +0530

    setuptools install script

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+	name="xmlutils",
+	version="0.9",
+	description="A set of utilities for processing XML documents and converting to other formats",
+	author="Kailash Nadh",
+	author_email="kailash.nadh@gmail.com",
+	url="http://nadh.in/code/xmlutils.py",
+	packages=['xmlutils'],
+	download_url="http://github.com/knadh/xmlutils.py",
+	license="MIT License",
+	entry_points = {
+		'console_scripts': [
+			'xml2sql = xmlutils.console:run_xml2sql',
+			'xml2csv = xmlutils.console:run_xml2csv',
+			'xml2json = xmlutils.console:run_xml2json'
+		],
+	},
+	classifiers=[
+		"Development Status :: 4 - Beta",
+		"Intended Audience :: Developers",
+		"Programming Language :: Python",
+		"Natural Language :: English",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python :: 2.6",
+		"Programming Language :: Python :: 2.7",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+		"Topic :: Database",
+		"Topic :: Software Development :: Libraries"
+	]
+)
