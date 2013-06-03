1beb808d60209654c90fdb62a1296a2a1f8dcaa5
Kailash Nadh <kailash.nadh@gmail.com>
2013-06-03 22:29:31 +0530

    Major refactoring. Changed into a class from a hardcoded commandline utility

--- xml2csv.py
+++ xml2csv.py
@@ -1,112 +1,119 @@
 ﻿"""
-    xml2csv.py
-    Kailash Nadh, http://nadh.in
-    October 2011
-    
-    License:        MIT License
-    Documentation:    http://nadh.in/code/xmlutils.py
+	xml2csv.py
+	Kailash Nadh, http://nadh.in
+	October 2011
+	
+	License:        MIT License
+	Documentation:    http://nadh.in/code/xmlutils.py
 """
 
-import argparse
 import codecs
 import xml.etree.ElementTree as et
 
-print """xml2csv.py by Kailash Nadh (http://nadh.in)
---help for help
+class xml2csv:
+	output_buffer = []
+	output = None
 
-"""
+	def __init__(self, input_file, output_file, encoding='utf-8'):
+		"""Initialize the class with the paths to the input xml file
+		and the output sql file
 
-# parse arguments
-parser = argparse.ArgumentParser(description='Convert an xml file to csv format.')
-parser.add_argument('--input', dest='input_file', required=True, help='input xml filename')
-parser.add_argument('--output', dest='output_file', required=True, help='output csv filename')
-parser.add_argument('--tag', dest='tag', required=True, help='the record tag. eg: item')
-parser.add_argument('--delimiter', dest='delimiter', default=', ', help='delimiter character. (default=, comma-space)')
-parser.add_argument('--ignore', dest='ignore', default='', nargs='+', help='list of tags to ignore')
-parser.add_argument('--no-header', dest='header', action='store_false', default=True, help='do not print csv header (default=False)')
-parser.add_argument('--encoding', dest='encoding', default='utf-8', help='character encoding (default=utf-8)')
-parser.add_argument('--limit', type=int, dest='limit', default=-1, help='maximum number of records to process')
-parser.add_argument('--buffer', type=int, dest='buffer', default='1000', help='number of records to keep in buffer before writing to disk (default=1000)')
-args = parser.parse_args()
+		Keyword arguments:
+		input_file -- input xml filename
+		output_file -- output sql filename
+		encoding -- character encoding
+		"""
 
-# output file handle
-output = codecs.open(args.output_file, "w", encoding=args.encoding)
+		# open the xml file for iteration
+		self.context = et.iterparse(input_file, events=("start", "end"))
 
-# open the xml file for iteration
-context = et.iterparse(args.input_file, events=("start", "end"))
-context = iter(context)
-
-# get to the root
-event, root = context.next()
-
-items = []
-header = []
-output_buffer = []
-field_name = ''
-
-tagged = False
-started = False
-n = 0
+		# output file handle
+		try:
+			self.output = codecs.open(output_file, "w", encoding=encoding)
+		except:
+			print("Failed to open the output file")
+			raise
 
 
-def write_buffer():
-    """
-    Write records from buffer to the output file
-    """
-    global output_buffer, output
-    
-    output.write('\n'.join(output_buffer) + '\n')
-    output_buffer = []
-    
-    print ".",
+	def convert(self, tag="item", delimiter=",", ignore=[], header=False,
+				limit=-1, buffer_size=1000):
+
+		"""Convert the XML file to SQL file
+
+			Keyword arguments:
+			tag -- the record tag. eg: item
+			delimiter -- csv field delimiter
+			ignore -- list of tags to ignore
+			limit -- maximum number of records to process
+			buffer -- number of records to keep in buffer before writing to disk
+
+			Returns:
+			number of records converted,
+		"""
+
+		# get to the root
+		event, root = self.context.next()
+
+		items = []
+		header_line = []
+		field_name = ''
+
+		tagged = False
+		started = False
+		n = 0
 
 
-def show_stats():
-    print "\n\nWrote", n, "records to", args.output_file
+		# iterate through the xml
+		for event, elem in self.context:
+			# if elem is an unignored child node of the record tag, it should be written to buffer
+			should_write = elem.tag != tag and started and elem.tag not in ignore
+			# and if a header is required and if there isn't one
+			should_tag = not tagged and should_write and header
 
-# iterate through the xml
-for event, elem in context:
-    # if elem is an unignored child node of the record tag, it should be written to buffer
-    should_write = elem.tag != args.tag and started and elem.tag not in args.ignore
-    # and if the user wants a header and we have not created one yet, we should tag it too
-    should_tag = not tagged and should_write and args.header
+			if event == 'start':
+				if elem.tag == tag and not started:
+					started = True
+				elif should_tag:
+					# if elem is nested inside a "parent", field name becomes parent_elem
+					field_name = '_'.join((field_name, elem.tag)) if field_name else elem.tag
 
-    if event == 'start':
-        if elem.tag == args.tag and not started:
-            started = True
-        elif should_tag:
-            # if elem is nested inside a "parent", field name becomes parent_elem
-            field_name = '_'.join((field_name, elem.tag)) if field_name else elem.tag
+			else:
+				if should_write:
+					if should_tag:
+						header_line.append(field_name)  # add field name to csv header
+						# remove current tag from the tag name chain
+						field_name = field_name.rpartition('_' + elem.tag)[0]
+					items.append('' if elem.text is None else elem.text.strip().replace('"', r'\"'))
 
-    else:
-        if should_write:
-            if should_tag:
-                header.append(field_name)  # add field name to csv header
-                # remove current tag from the tag name chain
-                field_name = field_name.rpartition('_' + elem.tag)[0]
-            items.append('' if elem.text is None else elem.text.strip().replace('"', r'\"'))
+				# end of traversing the record tag
+				elif elem.tag == tag and len(items) > 0:
+					# csv header (element tag names)
+					if header_line and not tagged:
+						self.output.write('#' + delimiter.join(header_line) + '\n')
+					tagged = True
 
-        # end of traversing the record tag
-        elif elem.tag == args.tag and len(items) > 0:
-            # csv header (element tag names)
-            if args.header and not tagged:
-                output.write('#' + args.delimiter.join(header) + '\n')
-            tagged = True
+					# send the csv to buffer
+					self.output_buffer.append(r'"' + (r'"' + delimiter + r'"').join(items) + r'"')
+					items = []
+					n += 1
 
-            # send the csv to buffer
-            output_buffer.append(r'"' + (r'"' + args.delimiter + r'"').join(items) + r'"')
-            items = []
-            n += 1
+					# halt if the specified limit has been hit
+					if n == limit:
+						break
 
-            # halt if the specified limit has been hit
-            if n == args.limit:
-                break
+					# flush buffer to disk
+					if len(self.output_buffer) > buffer_size:
+						self._write_buffer()
 
-            # flush buffer to disk
-            if len(output_buffer) > args.buffer:
-                write_buffer()
+				elem.clear()  # discard element and recover memory
 
-        elem.clear()  # discard element and recover memory
+		self._write_buffer()  # write rest of the buffer to file
 
-write_buffer()  # write rest of the buffer to file
-show_stats()
+		return n
+
+
+	def _write_buffer(self):
+		"""Write records from buffer to the output file"""
+
+		self.output.write('\n'.join(self.output_buffer) + '\n')
+		self.output_buffer = []
